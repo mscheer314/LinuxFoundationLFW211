d04796a2c3ba7cb14adafc11cb890fc0db66e18d
scheerm <mark.scheer@wwt.com>
2023-09-08 16:03:51 -0500

    add ch8 lab 2

--- ch-8/labs-2/serial.js
+++ ch-8/labs-2/serial.js
@@ -1,40 +1,49 @@
-'use strict'
-const { promisify } = require('util')
+"use strict";
+const { promisify } = require("util");
 
-const print = (err, contents) => { 
-  if (err) console.error(err)
-  else console.log(contents) 
-}
+const print = (err, contents) => {
+  if (err) console.error(err);
+  else console.log(contents);
+};
 
 const opA = (cb) => {
   setTimeout(() => {
-    cb(null, 'A')
-  }, 500)
-}
+    cb(null, "A");
+  }, 500);
+};
 
 const opB = (cb) => {
   setTimeout(() => {
-    cb(null, 'B')
-  }, 250)
-}
+    cb(null, "B");
+  }, 250);
+};
 
 const opC = (cb) => {
   setTimeout(() => {
-    cb(null, 'C')
-  }, 125)
-}
+    cb(null, "C");
+  }, 125);
+};
 
 const opAPromise = promisify(opA)
 const opBPromise = promisify(opB)
 const opCPromise = promisify(opC)
 
-opAPromise().then(resultA => {
-  print(null, resultA)
-  opBPromise().then(resultB => {
-    print(null, resultB)
-    opCPromise().then(resultC => {
-      print(null, resultC)
-    })
-  })
-})
+// version with promises
+// opAPromise().then(resultA => {
+//   print(null, resultA)
+//   opBPromise().then(resultB => {
+//     print(null, resultB)
+//     opCPromise().then(resultC => {
+//       print(null, resultC)
+//     })
+//   })
+// })
 
+// version with async/await
+async function run() {
+  const thing1 = await opAPromise(print);
+  const thing2 = await opBPromise(print);
+  const thing3 = await opCPromise(print);
+}
+
+run();
