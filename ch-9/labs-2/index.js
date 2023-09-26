c4d621a0b22594ec27a466ae753ed1f6135cc25c
scheerm <mark.scheer@wwt.com>
2023-09-26 11:03:49 -0500

    complete ch9 lab 2

--- ch-9/labs-2/index.js
+++ ch-9/labs-2/index.js
@@ -1,8 +1,14 @@
+// Currently the process crashes. Without removing any of the existing code, and without using
+// a try/catch block add some code which stops the process from crashing. When implemented correctly
+// the process should output passed!
 'use strict'
 const { EventEmitter } = require('events')
 
 process.nextTick(console.log, 'passed!')
 
 const ee = new EventEmitter()
+ee.on('error', function() {
+    process.stdin.resume()
+})
 
 ee.emit('error', Error('timeout'))
