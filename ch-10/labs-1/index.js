771b81e7f00bed11fd2b92897e204725bc9d9932
scheerm <mark.scheer@wwt.com>
2023-10-12 17:08:15 -0500

    finish ch 10 lab 1

--- ch-10/labs-1/index.js
+++ ch-10/labs-1/index.js
@@ -1,15 +1,18 @@
-'use strict'
-const assert = require('assert')
+"use strict";
+const assert = require("assert");
 
-function parseUrl (str) {
-  const parsed = new URL(str)
-  return parsed
+function parseUrl(str) {
+  try {
+    const parsed = new URL(str);
+    return parsed;
+  } catch (error) {
+    return null;
+  }
 }
 
-assert.doesNotThrow(() => { parseUrl('invalid-url') })
-assert.equal(parseUrl('invalid-url'), null)
-assert.deepEqual(
-  parseUrl('http://example.com'), 
-  new URL('http://example.com')
-)
-console.log('passed!')
+assert.doesNotThrow(() => {
+  parseUrl("invalid-url");
+});
+assert.equal(parseUrl("invalid-url"), null);
+assert.deepEqual(parseUrl("http://example.com"), new URL("http://example.com"));
+console.log("passed!");
