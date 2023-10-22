7c72d3d0307fb6f603731e2505820a5bd4a1fefb
scheerm <mark.scheer@wwt.com>
2023-10-22 16:25:29 -0500

    complete ch10 lab 2

--- ch-10/labs-2/index.js
+++ ch-10/labs-2/index.js
@@ -1,24 +1,24 @@
-'use strict'
-const { readFileSync } = require('fs')
-const { readFile } = require('fs/promises')
-const assert = require('assert')
+"use strict";
+const { readFileSync } = require("fs");
+const { readFile } = require("fs/promises");
+const assert = require("assert");
 
-async function read (file) {
-  const content = await readFile(file)
-  return content
+async function read(file) {
+  try {
+    const content = await readFile(file);
+    return content;
+  } catch (e) {
+    throw new Error("failed to read");
+  }
 }
 
-
-async function check () {
+async function check() {
   await assert.rejects(
-    read('not-a-valid-filepath'), 
-    new Error('failed to read')
-  )
-  assert.deepEqual(
-    await read(__filename),
-    readFileSync(__filename)
-  )
-  console.log('passed!')
+    read("not-a-valid-filepath"),
+    new Error("failed to read"),
+  );
+  assert.deepEqual(await read(__filename), readFileSync(__filename));
+  console.log("passed!");
 }
 
-check()
+check();
