51b482544a0ee52b8d80d81ea118fb7dafb92b0a
scheerm <mark.scheer@wwt.com>
2023-10-22 17:00:34 -0500

    complete ch11 lab 1

--- ch-11/labs-1/index.js
+++ ch-11/labs-1/index.js
@@ -1,7 +1,7 @@
-'use strict'
-const assert = require('assert')
-const buffer = Buffer.allocUnsafe(4096)
-console.log(buffer)
+"use strict";
+const assert = require("assert");
+const buffer = Buffer.alloc(4096);
+console.log(buffer);
 
-for (const byte of buffer) assert.equal(byte, 0)
-console.log('passed!')
+for (const byte of buffer) assert.equal(byte, 0);
+console.log("passed!");
