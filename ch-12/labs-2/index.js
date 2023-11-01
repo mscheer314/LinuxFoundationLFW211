f1cd439bde0e145ddcee0c208938ea6714bb991f
scheerm <mark.scheer@wwt.com>
2023-11-01 14:58:40 -0500

    complete ch12 lab2

--- ch-12/labs-2/index.js
+++ ch-12/labs-2/index.js
@@ -1,27 +1,40 @@
-'use strict'
-const { Readable, Writable, Transform, PassThrough, pipeline } = require('stream')
-const assert = require('assert')
+"use strict";
+const {
+  Readable,
+  Writable,
+  Transform,
+  PassThrough,
+  pipeline,
+} = require("stream");
+const assert = require("assert");
 const createWritable = () => {
-  const sink = []
+  const sink = [];
   const writable = new Writable({
     write(chunk, enc, cb) {
-      sink.push(chunk.toString())
-      cb()
-    }
-  })
-  writable.sink = sink
-  return writable
-}
-const readable = Readable.from(['a', 'b', 'c'])
-const writable = createWritable()
+      sink.push(chunk.toString());
+      cb();
+    },
+  });
+  writable.sink = sink;
+  return writable;
+};
+const readable = Readable.from(["a", "b", "c"]);
+const writable = createWritable();
 
-// TODO: replace the pass through stream 
+// TODO: replace the pass through stream
 // with a transform stream that uppercases
 // incoming characters
-const transform = new PassThrough()
+// const transform = new PassThrough() REPLACED
+
+const transform = new Transform({
+  transform(chunk, enc, cb) {
+    const transformedChunk = chunk.toString().toUpperCase();
+    cb(null, transformedChunk);
+  },
+});
 
 pipeline(readable, transform, writable, (err) => {
-  assert.ifError(err)
-  assert.deepStrictEqual(writable.sink, ['A', 'B', 'C'])
-  console.log('passed!')
-})
+  assert.ifError(err);
+  assert.deepStrictEqual(writable.sink, ["A", "B", "C"]);
+  console.log("passed!");
+});
