85f6e59b18ee88dfec3d6acaed1fa0a4a00c76b0
scheerm <mark.scheer@wwt.com>
2023-12-29 11:41:23 -0600

    update ch 16

--- ch-16/labs-1/uppercase.spec.js
+++ ch-16/labs-1/uppercase.spec.js
@@ -2,6 +2,14 @@
 
 const uppercase = require("./uppercase");
 
+test("[uppercase] throws Error if the input is not a string", () => {
+  expect(() => uppercase(1).toThrowError(
+    Error('input must be a string')
+  ))
+});
+
 test("[uppercase] converts string to uppercase", () => {
   expect(uppercase("asdf")).toStrictEqual("ASDF");
 });
+
+
